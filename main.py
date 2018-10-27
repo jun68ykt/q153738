ebe968afb349611d282e55351a60b4bfccf9080d
jun68ykt <jun68ykt@teratail>
2018-10-27 23:11:55 +0900

    正規表現を修正

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from dbfread import DBF
-import re
+import regex as re
+
+pattern = '^\P{C}'
 
 for record in DBF('zaliznia.dbf', encoding='cp1251', char_decode_errors='replace'):
-    word = record['WORD'][0] if re.match('^\w', record['WORD']) else None
+    word = record['WORD'][0] if re.match(pattern, record['WORD']) else None
     if word:
-        grammar = record['GRAMMAR'][0] if re.match('^\w', record['GRAMMAR']) else ''
-        trans = record['TRANS'][0] if re.match('^\w', record['TRANS']) else ''
+        grammar = record['GRAMMAR'][0] if re.match(pattern, record['GRAMMAR']) else ''
+        trans = record['TRANS'][0] if re.match(pattern, record['TRANS']) else ''
         print(','.join([word, grammar, trans]))
